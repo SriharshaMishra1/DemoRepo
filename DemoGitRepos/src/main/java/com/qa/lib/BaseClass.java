0bf1596a0b2c909aab71529d2b496f0cc17eb8aa
sriharsha <sriharsha@sriharsha-PC>
2019-02-20 19:26:26 +0530

    New Demo pushed to git Acc

---
+++ DemoGitRepos/src/main/java/com/qa/lib/BaseClass.java
@@ -0,0 +1,9 @@
+package com.qa.lib;
+
+public class BaseClass {
+
+	public void Test()
+	{
+		System.out.println("Test Method");
+	}
+}
---
+++ DemoGitRepos/src/main/java/com/qa/utils/TestUtils.java
@@ -0,0 +1,10 @@
+package com.qa.utils;
+
+public class TestUtils {
+	
+	public void utils()
+	{
+		System.out.println("test utils");
+	}
+
+}
